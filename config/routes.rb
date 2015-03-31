d8fd7b2ed463cfea3b9d180fab8a3ce0d21f63b8
carlos99 <carlos.cw2106@gmail.com>
2015-03-31 10:49:41 -0600

    Added votes functionlity. Now you can vote a link. The views was changes too.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  
   devise_for :users
-  resources :links
+  resources :links do
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+    end
+
+  end
 
   root "links#index"
 
